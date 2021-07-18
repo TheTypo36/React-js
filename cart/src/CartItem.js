a2718dad719ff3e3fc63f60b533a31be9283b0e8
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-07-18 17:36:19 +0530

    added one react component that is cartItem along with some css

---
+++ cart/src/CartItem.js
@@ -0,0 +1,32 @@
+import { getSuggestedQuery } from '@testing-library/react';
+import React from 'react';
+
+class CartItem extends React.Component {
+    render() {
+        return (
+            <div className="cart-item">
+                <div class="left-block">
+                    <img style={style.image} />
+                </div>
+                <div className="right-block">
+                    <div style={{ fontSize: 25 }}>Phone</div>
+                    <div style={{ color: '#777' }}>Rs 999</div>
+                    <div style={{ color: '#777' }}>Qty: 1</div>
+                    <div className=".cart-item-actions">
+                        {/* buttons */}
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+const style = {
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        backgroundColor: 'grey'
+    }
+}
+export default CartItem;
