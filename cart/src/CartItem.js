571dd7d4f4e1b6bdfb1dde60103b9aac2ab9c34b
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-07-18 22:50:36 +0530

    added state for maintaining data of cart items and also added the 3 action buttons in the cart item

--- cart/src/CartItem.js
+++ cart/src/CartItem.js
@@ -2,18 +2,31 @@ import { getSuggestedQuery } from '@testing-library/react';
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+            price: 999,
+            title: 'phone',
+            qty: '1',
+            img: ''
+        }
+    }
     render() {
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
-                <div class="left-block">
+                <div className="left-block">
                     <img style={style.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 25 }}>Phone</div>
-                    <div style={{ color: '#777' }}>Rs 999</div>
-                    <div style={{ color: '#777' }}>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price}</div>
+                    <div style={{ color: '#777' }}>Qty: {qty}</div>
                     <div className=".cart-item-actions">
-                        {/* buttons */}
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" />
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" />
+                        <img alt="remove" className="action-icons" src="https://image.flaticon.com/icons/png/512/1214/1214428.png" />
                     </div>
                 </div>
 
