4e47eef5a60920360402165399f21a6a76ca5f03
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-07-19 13:07:38 +0530

    add decrease quantity state modification

--- cart/src/CartItem.js
+++ cart/src/CartItem.js
@@ -14,13 +14,32 @@ class CartItem extends React.Component {
     }
     increaseQuantity = () => {
         // this.state.qty = this.state.qty + 1;
+
+        //this is 1 form of setState where you're not concern with the prev state of component.
         // this.setState({
         //     qty: this.state.qty + 1
         // });
-        console.log(this.state);
+        //console.log(this.state);
+
+        //this is 2 form of setState where you use prevState to modify the value and change it to new value.
         this.setState((prevState) => {
             return {
                 qty: prevState.qty + 1
+            }
+        });
+
+    }
+
+    decreaseQuantity = () => {
+        this.setState((prevState) => {
+            if (prevState.qty != 0) {
+                return {
+                    qty: prevState.qty - 1
+                }
+            } else {
+                return {
+                    qty: prevState.qty
+                }
             }
         });
     }
@@ -42,7 +61,13 @@ class CartItem extends React.Component {
                             src="https://image.flaticon.com/icons/png/512/992/992651.png"
                             onClick={this.increaseQuantity}
                         />
-                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" />
+                        <img
+                            alt="decrease"
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/png/512/992/992683.png"
+                            onClick={this.decreaseQuantity}
+                        />
+
                         <img alt="remove" className="action-icons" src="https://image.flaticon.com/icons/png/512/1214/1214428.png" />
                     </div>
                 </div>
