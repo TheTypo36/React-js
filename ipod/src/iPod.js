06e36ab1fe865a5887b24a5a231ea70960b91b6d
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-07-27 18:21:23 +0530

    create react app ipod with intial directory system

---
+++ ipod/src/iPod.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const iPod = () => {
+    return (
+        <div className="ipod">
+            <div id="screen">
+
+                <div id="menu-btns">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const style = {
+    height: 200,
+    width: 
+}
