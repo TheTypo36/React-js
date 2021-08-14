a7cf2e8af1bc52e4597963f671d3ca28e27c7858
Ashish Raghuvanshi <thetypo36@gmail.com>
2021-08-14 16:36:45 +0530

    add a action create for adding movies

--- movie-app/src/action/index.js
+++ movie-app/src/action/index.js
@@ -1,4 +1,16 @@
-{
-    type: 'ADD_MOVIES'
-    Movie: [m1, m2, m3];
+// {
+//     type: 'ADD_MOVIES'
+//     Movie: [m1, m2, m3];
+// }
+
+//action types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+
+//action  creators
+export default function addMovie(movies) {
+    return ({
+        type: ADD_MOVIES,
+        movies
+    });
 }
