910917bf1805c23fc83e0f8e00d1d4fef62a48f0
Ashish Raghuvanshi <thetypo36@gmail.com>
2022-01-02 18:01:18 +0530

    fetch the post data from firestore and showed it in home using useeffect

--- react hook/react-hooks-blog/src/components/Home.js
+++ react hook/react-hooks-blog/src/components/Home.js
@@ -1,5 +1,38 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { firestore } from '../firebase';
 function Home() {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        console.log('posts', posts);
+        setPosts(posts);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Ashish Raghuvanshi</div>
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.subtitle}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
